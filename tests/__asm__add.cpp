7f1d51f98614a220bdaa8554873dc874030c4c7c
Romeo Sarkar <romeosarkar10x@gmail.com>
2025-07-14 01:34:47 +0530

    setup clangd, clang-format, etc.

---
+++ tests/__asm__add.cpp
@@ -0,0 +1,54 @@
+#include "../Include/__asm__add.hpp"
+
+int main()
+{
+  {
+    // Test 1:
+    volatile u_ll a[] = { 1, 243, 3, 4, 5, 6, 0 }, b[] = { 7, 8, 9, 10, 11, 12, 0 }, size__b = sizeof(b) / sizeof(u_ll);
+
+    __asm__add(a, b, size__b);
+    
+    for(u_ll u = 0; u < size__b; u++) { std::cout << a[u] << " "; }
+    std::cout << std::endl;
+
+    // Expected Output: 8 251 12 14 16 18 0
+  }
+
+  {
+    // Test 2:
+    volatile u_ll a[] = { 3, 0, 0, 1, 3, 1, 0 }, b[] = { 2, 0, 0, ULONG_LONG_MAX, ULONG_LONG_MAX, 12, 0 }, size__b = sizeof(b) / sizeof(u_ll);
+
+    __asm__add(a, b, size__b);
+    
+    for(u_ll u = 0; u < size__b; u++) { std::cout << a[u] << " "; }
+    std::cout << std::endl;
+
+    // Expected Output: 5 0 0 0 3 14 0
+  }
+
+  {
+    // Test 3:
+    volatile u_ll a[] = { 3, 0 }, b[] = { ULONG_LONG_MAX, 0 }, size__b = sizeof(b) / sizeof(u_ll);
+
+    __asm__add(a, b, size__b);
+    
+    for(u_ll u = 0; u < size__b; u++) { std::cout << a[u] << " "; }
+    std::cout << std::endl;
+
+    // Expected Output: 2 1
+  }
+
+  {
+    // Test 4:
+    volatile u_ll a[] = {6512548990859393274, 1, 0 }, b[] = { 216110626640441270, 0, 0  }, size__b = sizeof(b) / sizeof(u_ll);
+
+    __asm__add(a, b, size__b);
+    
+    for(u_ll u = 0; u < size__b; u++) { std::cout << a[u] << " "; }
+    std::cout << std::endl;
+
+    // Expected Output: 6728659617499834544 1 0
+  }
+
+  return 0;
+}
